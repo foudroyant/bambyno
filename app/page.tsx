7677b0bd31a83ba1c313e8c883bdc8e2592546f7
Foudroyant <stephanebazebibouta@gmail.com>
2025-04-29 12:08:34 +0100

    ajout du chatbot n8n

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import FloatingTechIcons from "@/components/FloatingTechIcons/view";
 import ParallaxSection from "@/components/ParallaxSection/view";
 import Testimonials from "@/components/Testimonials/view";
@@ -8,7 +9,29 @@ import Hero from "@/components/hero/view";
 import Services from "@/components/services/view";
 import Image from "next/image";
 
+
+import '@n8n/chat/style.css';
+import { createChat } from '@n8n/chat';
+import { useEffect } from 'react';
+
 export default function Home() {
+
+  useEffect(() => {
+		createChat({
+			webhookUrl: 'https://n8n.bambyno.xyz/webhook/9d40707f-ae02-43f7-8a6d-70c0a3d0db93/chat',
+      target: '#n8n-chat',
+      mode: 'window',
+      chatInputKey: 'chatInput',
+      chatSessionKey: 'sessionId',
+      metadata: {},
+      showWelcomeScreen: false,
+      defaultLanguage: 'en',
+      initialMessages: [
+        "💡 Bienvenue ! Je suis Foudroyant, l'assistant intelligent de Bambyno. Pouvez poser vos questions sur nos services y compris demander un devis.👋",
+      ],
+		});
+	}, []);
+
   return (
     <>
     <Hero />
